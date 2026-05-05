5982f31615c206204b05674190ed3af5528bd4d4
David Rogala <34407302+DaveRogala@users.noreply.github.com>
2026-05-05 09:55:54 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿
+using GenericRepositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using VCFFileImport.Contracts;
+using VCFFileImport.Data;
+using VCFFileImport.Models.Database;
+using VCFFileImport.Repositories;
+using VCFFileImport.Services;
+using MagellanFileServices.Services;
+using MagellanFileServices.Contracts;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+ILogger logger = builder.Services.BuildServiceProvider().GetRequiredService<ILogger<Program>>();
+
+builder.Services.AddSingleton(builder.Configuration);
+
+builder.Services.AddDbContextFactory<VcfContext>(opt =>
+    opt.UseSqlServer(builder.Configuration["ConnectionStrings:DefaultConnection"]));
+
+builder.Services.AddScoped<IGenericRepository<ImportFile, VcfContext,int>, ImportFileRepository> ();
+builder.Services.AddScoped<IFileServices, FileServices>();
+
+builder.Services.AddScoped<IVcfServices, VcfServices>();
+
+using IHost host = builder.Build();
+
+IVcfServices? services = host.Services.GetRequiredService<IVcfServices>();
+
+if(services != null)
+{
+    if(!await services.ProcessFileAsync())
+    {
+        throw new Exception("file process failed.  See errors folder");
+    }
+}
+else
+{
+    logger.LogCritical("VcfServices failed to start");
+    throw new Exception("VcfServices failed to start");
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# VCF Feed Import
